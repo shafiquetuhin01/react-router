6e0e49f60fe43e727edc27623122d5541a4b5747
Shafique <sfqtuhin04@gmail.com>
2022-03-31 07:07:56 +0600

    updating custom link

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
-function CustomLink({ children, to, ...props }: import("react-router-dom").LinkProps) {
-    let resolved = useResolvedPath(to);
-    let match = useMatch({ path: resolved.pathname, end: true });
-  
-    return (
-      <div>
-        <Link
-          style={{ textDecoration: match ? "underline" : "none" }}
-          to={to}
-          {...props}
-        >
-          {children}
-        </Link>
-        {match && " (active)"}
-      </div>
-    );
-  }
+function CustomLink({ children, to, ...props }) {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <div>
+      <Link
+        style={{
+          color: match ? "red" : "black",
+          textDecoration: match ? "underline" : "none",
+        }}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+      {match}
+    </div>
+  );
+}
 
 export default CustomLink;
